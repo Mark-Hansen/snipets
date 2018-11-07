89afed350142617a0f473679f6c7392e89c16617
Mark Hansen <markhansen@Mark-Hansen.local>
2018-11-07 14:54:51 +1100

    Added generate migration

--- ruby/rails_ar_useful.rb
+++ ruby/rails_ar_useful.rb
@@ -1,4 +1,8 @@
 
+script/rails generate migration AddSourceSourceIdentifierToNboProductTables
+# RETURNS
+#  invoke  active_record
+#  create    db/migrate/20181107005836_add_source_source_identifier_to_nbo_product_tables.rb
 
 mike [12:56 PM]: I thought scenic had a single method for updating views
 
